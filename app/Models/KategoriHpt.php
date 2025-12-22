ac8bed10b53e4fef6beee3bf52d100744778441d
InesPrabandari <gabyzeebafelyn@gmail.com>
2025-12-23 00:20:26 +0700

    Membuat model kategori HPT

---
+++ app/Models/KategoriHpt.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriHpt extends Model
+{
+    protected $table = 'kategori_hpt';
+    protected $fillable = ['nama_kategori'];
+}
