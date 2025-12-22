f46b5258dd02627d6d6cfca4e59a9f04d61d18cc
InesPrabandari <gabyzeebafelyn@gmail.com>
2025-12-23 00:21:51 +0700

    Membuat model blok lahan

---
+++ app/Models/BlokLahan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlokLahan extends Model
+{
+    protected $table = 'blok_lahan';
+    protected $fillable = ['nama_blok'];
+}
