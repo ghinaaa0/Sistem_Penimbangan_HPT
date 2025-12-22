454a214605fede6faf57c27e4dabae0681741ae4
InesPrabandari <gabyzeebafelyn@gmail.com>
2025-12-23 00:40:53 +0700

    Membuat routes untuk Input Data HPT

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HptController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/forgot-password', function () {
+    return 'Halaman lupa password nanti di sini.';
+})->name('password.request');
+
+Route::resource('hpt', HptController::class);
