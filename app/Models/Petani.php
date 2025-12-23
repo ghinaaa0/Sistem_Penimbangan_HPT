bb17497905e20b322ac7fad5172e6150c2ee3b50
sheeaa <118061171+sheeaa@users.noreply.github.com>
2025-12-23 13:24:18 +0700

    Create petani index blade view

--- app/Models/Petani.php
+++ app/Models/Petani.php
@@ -6,16 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Petani extends Model
 {
-    use HasFactory, Notifiable;
-
     protected $table = 'petani';
-
     protected $primaryKey = 'id_petani';
 
     protected $fillable = [
         'nama',
         'alamat',
-        'no_hp'
+        'no_hp',
     ];
-
 }
+
